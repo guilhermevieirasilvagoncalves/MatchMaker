b61f2f8c043d080798bf5da2ffa5a61c71aa0bc4
Guilherme Vieira <88863957+guilhermevieirasilvagoncalves@users.noreply.github.com>
2023-05-28 18:25:37 -0300

    Update query.sql

--- Banco de Dados/query.sql
+++ Banco de Dados/query.sql
@@ -1,4 +1,4 @@
 SELECT AVG(ag.preco) média, q.esporte, ag.tipo from agendamento as ag
 INNER JOIN quadra q on q.id_quadra = ag.id_quadra
 WHERE EXTRACT(YEAR from ag.data) = 2023
-GROUP BY EXTRACT(YEAR from ag.data), q.esporte, ag.tipo
+GROUP BY EXTRACT(MONTH from ag.data), q.esporte, ag.tipo
